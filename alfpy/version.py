e0782e9551458ef17ab29df8af13fc0f8925e894
Andrzej Zielezinski <a.zielezinski@gmail.com>
2018-03-13 08:33:02 +0100

    version 1.0.6

--- alfpy/version.py
+++ alfpy/version.py
@@ -2,4 +2,4 @@
 # 1) I don't load dependencies by storing it in __init__.py
 # 2) I can import it in setup.py for the same reason.
 # 3) I can import it into any module.
-__version__ = '1.0.5'
+__version__ = '1.0.6'
